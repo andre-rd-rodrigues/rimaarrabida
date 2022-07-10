fbb2669044e296abc9d8b7d17e55a812a3f8ea66
andrerodrigues <andrerodrigues29101992@gmail.com>
2022-07-10 21:18:09 +0100

    Updates

--- src/App/__tests__/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-describe("App", () => {
-  it("should render correctly", () => {
-    render(<App />);
-  });
-});
--- src/components/AppIcon/AppIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import FeatherIcon from "feather-icons-react";
-import colors from "styles/_mixins.scss";
-
-const AppIcon = ({ icon, size = 20, color = "primary" }) => {
-  return <FeatherIcon icon={icon} size={size} color={colors[color]} />;
-};
-
-export default AppIcon;
